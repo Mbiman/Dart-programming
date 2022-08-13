fb482eabd4559b8e32a3ea8ec0d47f2a2b10f5c6
mbiman <juniormbi80@gmail.com>
2022-08-13 02:51:13 +0100

    initial commit

---
+++ hello.dart
@@ -0,0 +1,13 @@
+/**
+ * a multiline comment
+ */
+
+void main() {
+  //first line of code
+  print("Hello world");
+  print("This is my first application");//another line of code
+  print(50);
+  //performing arithmetic operations
+  print(4 - 1);
+  print(true);//printing a boolean value
+}
