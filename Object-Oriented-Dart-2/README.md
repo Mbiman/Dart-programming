7b8edc4f91f9fbaab7cc306c44f43bc188e8ed70
mbiman <juniormbi80@gmail.com>
2022-08-15 22:07:45 +0100

    updated

---
+++ Object-Oriented-Dart-2/README.md
@@ -0,0 +1,9 @@
+The Section covers:
+    
+    getter and setters: Default and Custom
+    Inheritance in Dart
+    Method Overriding
+    Using Constructors during Inheritance
+    Abstract Classes and Methods
+    Using Interface in Dart
+    Static Methods and Variables
---
+++ Object-Oriented-Dart-2/gettersAndSetters.dart
@@ -0,0 +1,27 @@
+//OBJ
+/**
+ * 1. Default Getter and Setter
+ * 2. Custom Getter and Setter
+ * 3. Private Instance Variable
+ */
+
+void main(List<String> args) {
+  var student = Student();
+  student.name = "Peter"; //calling default setter to set the value
+  print(student.name); //calling default Gettr to get value
+
+  student.percentage = 439; //calling custom setter to set value
+
+  print(student.percentage);
+}
+
+class Student {
+  late String name; //Instance Variable with default getter and setter
+
+  late double _percent; //private Instance variable for its own library
+
+  //instance variable with custom setter
+  void set percentage(double marksSecured) => _percent = (marksSecured / 500) * 100;
+  //instance variable with custom getter
+  double get percentage => _percent;
+}
