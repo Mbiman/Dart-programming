1c8c8e7d108f4d15f25f0f17143ca43c4716a414
mbiman <juniormbi80@gmail.com>
2022-08-15 10:09:30 +0100

    whileloops

---
+++ Loop-Control-Statements/WHILEloop.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  //WHILE loop
+  //find the even numbers between 1 to 10
+
+  int i = 1;
+  while (i <= 10) {
+    if (i % 2 == 0) {
+      print(i);
+    }
+    i++;
+  }
+}
