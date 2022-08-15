32f2c064c9556dbe9c847e0ef53d2daeb50215bf
mbiman <juniormbi80@gmail.com>
2022-08-15 10:15:42 +0100

    dowhileloops

---
+++ Loop-Control-Statements/DO-WHILEloop.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  // DO-WHILE loop
+  // find the odd numbers between 1 to 10
+
+  int i = 1;
+  do {
+    if (i % 2 != 0) {
+      print(i);
+    }
+    i++;
+  } while (i <= 10);
+}
